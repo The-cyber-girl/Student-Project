9f0b4324cd2f2b7cfbec508db3a0a4a69d5585c5
The-cyber-girl <bazzibusinessf@gmail.com>
2026-03-29 20:40:51 +0300

    Add files via upload

---
+++ myMain.java
@@ -0,0 +1,16 @@
+package chatGpt_questions;
+
+public class myMain {
+
+	public static void main(String[] args) {
+
+		student Student = new student("fatima",18);
+         Student.info();
+		
+        student student2 = new student("maria", 18);
+        student2.info();
+
+
+	}
+
+}
---
+++ student.java
@@ -0,0 +1,21 @@
+package chatGpt_questions;
+
+public class student {
+	
+	String name= "fatima";
+	int age = 18;
+	
+	student(String name,int age){
+		this.name =name;
+		this.age=age;
+	}
+		void info() {
+			System.out.println(name);
+			System.out.println(age);
+
+			
+		}
+	}
+	
+
+
